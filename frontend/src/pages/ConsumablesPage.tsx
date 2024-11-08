66154560877a2fff460b5bff5525584d658d5aef
namangoyal-21 <106027446+namangoyal-21@users.noreply.github.com>
2024-11-08 05:54:33 +0530

    Update ConsumablesPage.tsx

--- frontend/src/pages/ConsumablesPage.tsx
+++ frontend/src/pages/ConsumablesPage.tsx
@@ -109,7 +109,7 @@ const ConsumablesPage: React.FC = () => {
                     {typeof consumable.vendor === 'string' ? consumable.vendor : consumable.vendor?.name}
                   </td>
                   <td className="py-3 px-4 text-gray-800">
-                     {consumable.consumableCategory?.name || 'N/A'}
+                     {consumable?.categoryName || 'N/A'}
                   </td>
                 </tr>
               ))
