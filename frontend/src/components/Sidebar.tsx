cf0e731129b994e8dd3d8cbccc129bc468935596
namangoyal-21 <106027446+namangoyal-21@users.noreply.github.com>
2024-11-13 18:22:09 +0530

    Update Sidebar.tsx

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,10 @@
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { useNavigate, Link } from "react-router-dom";
 import { MdOutlineInventory } from "react-icons/md";
-import { FaSignOutAlt } from "react-icons/fa"; // Import new icon for OUT page
+import { FaSignOutAlt, FaUserPlus } from "react-icons/fa";
+import { IoMdPeople } from "react-icons/io";
+import { GrTransaction } from "react-icons/gr";
+import { TbCategoryPlus } from "react-icons/tb";
 
 interface SidebarProps {
   isOpen: boolean;
@@ -23,12 +27,17 @@ const SidebarComponent: React.FC<SidebarProps> = ({ isOpen }) => {
 
   return (
     <div
-      className={`fixed z-10 top-16 left-0 w-64 h-[calc(100vh-4rem)] bg-gray-100 text-gray-900 flex flex-col shadow-md transform transition-transform duration-300 ease-in-out ${isOpen ? "translate-x-0" : "-translate-x-full"}`}
+      className={`fixed z-10 top-16 left-0 w-64 h-[calc(100vh-4rem)] bg-gray-100 text-gray-900 flex flex-col shadow-md transform transition-transform duration-300 ease-in-out ${
+        isOpen ? "translate-x-0" : "-translate-x-full"
+      }`}
     >
       <nav className="flex flex-col flex-grow mt-4">
-        <NavItem href="/consumables" icon={<MdOutlineInventory />} label="Add Consumables" />
-        <NavItem href="/out" icon={<FaSignOutAlt />} label="Claim Consumable" /> {/* Add new link for OUT */}
-        <NavItem href="/history" icon={<MdOutlineInventory />} label="Transaction History" />
+        <NavItem to="/consumables" icon={<MdOutlineInventory />} label="Add Consumables" />
+        <NavItem to="/out" icon={<FaSignOutAlt />} label="Claim Consumable" />
+        <NavItem to="/history" icon={<GrTransaction />} label="Transaction History" />
+        <NavItem to="/vendors" icon={<FaUserPlus />} label="Add Vendor" />
+        <NavItem to="/people" icon={<IoMdPeople />} label="Add User" />
+        <NavItem to="/category" icon={<TbCategoryPlus />} label="Add Category" />
       </nav>
       <div className="px-4 py-3 border-t border-gray-300">
         <button
@@ -42,14 +51,18 @@ const SidebarComponent: React.FC<SidebarProps> = ({ isOpen }) => {
   );
 };
 
-const NavItem: React.FC<{ href: string; icon: React.ReactNode; label: string }> = ({ href, icon, label }) => (
-  <a
-    href={href}
+const NavItem: React.FC<{
+  to: string;
+  icon: React.ReactNode;
+  label: string;
+}> = ({ to, icon, label }) => (
+  <Link
+    to={to}
     className="flex items-center px-4 py-3 hover:bg-gray-200 rounded-lg mx-3 mb-2 transition"
   >
     <span className="mr-3">{icon}</span>
     <span className="text-lg font-semibold">{label}</span>
-  </a>
+  </Link>
 );
 
 export default SidebarComponent;
