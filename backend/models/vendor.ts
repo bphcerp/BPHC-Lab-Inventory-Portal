31323a050f2394847dbdb41e25d974ee1506ee6c
namangoyal-21 <106027446+namangoyal-21@users.noreply.github.com>
2024-11-13 18:13:37 +0530

    Update vendor.ts

--- backend/models/vendor.ts
+++ backend/models/vendor.ts
@@ -2,8 +2,7 @@ import mongoose, { Schema } from 'mongoose';
 
 const vendorSchema = new Schema({
   name: { type: String, required: true, unique: true },
-//   contactInfo: { type: String },
-//   address: { type: String }
+  comment: { type: String },
 });
 
 export const VendorModel = mongoose.model('vendors', vendorSchema);
