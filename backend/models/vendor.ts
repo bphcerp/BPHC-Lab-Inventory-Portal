95f2584aa96105e887edffd97eb5a63e241467eb
namangoyal-21 <106027446+namangoyal-21@users.noreply.github.com>
2024-11-27 16:33:19 +0530

    Update vendor.ts

--- backend/models/vendor.ts
+++ backend/models/vendor.ts
@@ -1,6 +1,11 @@
 import mongoose, { Schema } from 'mongoose';
 
 const vendorSchema = new Schema({
+  vendorId: {
+    type: String,
+    unique: true,
+    default: () => `VND-${Date.now()}-${Math.floor(Math.random() * 1000)}`, // Generate a unique ID
+  },
   name: { type: String, required: true, unique: true },
   phone: { type: String, required: true },
   email: { type: String, required: true },
