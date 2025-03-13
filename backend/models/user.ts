68e77fdaa429853b88a3beca012703307d2169e4
Nakul042 <118599133+Nakul042@users.noreply.github.com>
2025-03-13 15:19:18 +0530

    Update user.ts

--- backend/models/user.ts
+++ backend/models/user.ts
@@ -1,9 +1,17 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
+export interface IUser extends Document {
+  name: string;
+  email: string;
+  pwd?: string;
+  role: string;
+}
+
 const userSchema = new Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },  // Unique index for email
-    pwd: { type: String, required: false }  // Optional password
+    pwd: { type: String, required: false },  // Optional password
+    role: { type: String, required: true, enum: ['admin', 'dashboard'], default: 'dashboard' }
 });
 
-export const UserModel = mongoose.model('User', userSchema);
+export const UserModel = mongoose.model<IUser>('User', userSchema);
